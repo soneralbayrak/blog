2e527497435749c30f73091bde5ae6f6da3175f5
Soner Albayrak <snralbayrak@gmail.com>
2025-10-26 12:38:48 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
 	base: '/blog',
 	integrations: [
 		starlight({
-			title: 'Notlarım | My Blog',
+			title: 'Kişisel Notlarım | My Blog',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
